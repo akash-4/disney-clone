3d1dda0e43e870a4afb64101a391ce4234f46696
Akash Agarwal <akashagarwal41200@gmail.com>
2021-05-04 23:06:33 +0530

    Auth based route management

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./components/Detail";
 import Login from "./components/Login";
+import { Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUserName } from "./features/user/userSlice";
+
 function App() {
+  const userName = useSelector(selectUserName);
   return (
     <div className="App">
       <Router>
@@ -15,11 +20,11 @@ function App() {
           <Route path="/detail/:id">
             <Detail />
           </Route>
-          <Route path="/">
-            <Home />
-          </Route>
           <Route path="/login">
             <Login />
+          </Route>
+          <Route path="/">
+            {!userName ? <Redirect to="/login" /> : <Home />}
           </Route>
         </Switch>
       </Router>
